382fa6157badb8ebfeded0a8b92373bc127bae27
comp666 <comp622012016@mail.ru>
2016-09-15 17:15:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var sumBellForGoTo={
+	"Atlantic":"66", 
+	"Num":"66",
+	"str_one":"text",
+	"logic":"true"
+}
+for (var lol in sumBellForGoTo) {
+  console.log("sumBellForGoTo." + lol + " = " + sumBellForGoTo[lol]);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
