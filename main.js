89fb223421e41f41bfe360ad42e348cf8316146f
comp666 <comp622012016@mail.ru>
2016-09-23 15:20:57 +0300

    first commit

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-/*
-var sumBellForGoTo={
-	"Atlantic":"66", 
-	"Num":"66",
-	"str_one":"text",
-	"logic":"true"
-}
-for (var lol in sumBellForGoTo) {
-  console.log("sumBellForGoTo." + lol + " = " + sumBellForGoTo[lol]);
-}
+function findall(a, x){
+var results =[];
+ len = a.length;
+ pos =0;
+ while(pos < len){
+	 pos = a.index0f(x, pos);
+	 if (pos === -1)break;
+	 results.push(pos);
+	 pos = pos + 1;
+ }
+ return results;
+ }
+ 
+ 
 	
-*/
 
-
-
-var obj=new Object()
-obj.name='Sergey';
-obj['age']=19;
-document.write(obj.name+'<br />');
-document.write(obj.age);
 
 	
 	
-	
+
 	
 	
 	
